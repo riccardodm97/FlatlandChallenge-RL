639a6989890e2c4704428af9e1238665d8cc3c4c
Riccardo De Matteo <51266633+riccardodm97@users.noreply.github.com>
2021-07-17 13:32:12 +0200

    added assertion to check instance of model

--- src/run/exec_handler.py
+++ src/run/exec_handler.py
@@ -99,6 +99,10 @@ class ExcHandler:
         return obs_wrapper, env, max_steps
     
     def handleAgent(self, agn_par : dict) -> Agent:
+        if self._env_par['obs']['class'] == 'DensityObs' :
+            assert agn_par['model_class'] == 'DuelingCNN'
+
+
         agent_class = getattr(agent_classes, agn_par['class'])
         agent : Agent = agent_class(self._obs_shape, self._action_size, agn_par, self._checkpoint, True if self._mode == 'eval' else False)
 
